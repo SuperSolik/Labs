fa8be6bbfacfd78fa0ad530befcd3d3172df10c7
SuperSolik <serpetr99@gmail.com>
2019-04-22 22:52:18 +0300

    python code + tests

---
+++ StringAlgs/lab1/source.py
@@ -0,0 +1,21 @@
+def prefix(string):
+    res = [0]
+    k = 0
+    for i in range(1, len(string)):
+        while k > 0 and string[i] != string[k]:
+            k = res[k-1]
+        if string[i] == string[k]:
+            k += 1
+        res.append(k)
+    return res
+
+
+def kmp(pattern, text):
+    pref = prefix(pattern+'|'+text)
+    res = []
+    pl = len(pattern)
+    tl = len(text)
+    for i in range(pl+1, tl+pl+1):
+        if pref[i] == pl:
+            res.append(i - 2*pl)
+    return res if res else [-1]
